992d528b680fc0395faf588bfc1f2a06e04ffa7e
Adrian-Caiza <adrian.caiza@epn.edu.ec>
2024-11-15 20:58:48 -0500

    3 Actualizacion

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         //sin parametros
+        System.out.println("      Valores sin parametro     ");
         Perro perro1=new Perro();
         perro1.mostrarMascotas();
         perro1.mostrarPerro();
 
         //padre e hija
+        System.out.println("  Valores solo con padre e hija     ");
         Gato gato1= new Gato("Silvestre", 4,"perza","agudo");
         gato1.mostrarMascotas();
         gato1.mostrarGato();
 
         //solo padre
+        System.out.println("      Valores solo con padre     ");
         Tortuga tortuga1= new Tortuga("Rayo",10);
         tortuga1.mostrarMascotas();
         tortuga1.mostrarTortuga();
 
         //solo hija
+        System.out.println("      Valores solo con hija     ");
         Hamster hamster1= new Hamster("britanico", true);
         hamster1.mostrarMascotas();
         hamster1.mostrarHamster();
 
         //sin parametros y seteo
+        System.out.println("     Sin valores y seteo     ");
         Pajaro pajaro1= new Pajaro();
         pajaro1.setRaza("rapaz");
         pajaro1.setTamañoPico(2);
